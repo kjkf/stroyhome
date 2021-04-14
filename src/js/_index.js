54b2a8223b486bd6ad58d4aadddb7685ad4b9552
Lola <yakjkf@gmail.com>
2021-04-14 19:31:07 +0600

    blocks 10

---
+++ src/js/_index.js
@@ -0,0 +1,17 @@
+
+document.addEventListener('DOMContentLoaded', function (evt) {
+    init_menu();
+    initTabs();
+    initModal();
+    init_titles_anim();
+    initQuiz();
+    initSchemasDescr();
+    init_slider();
+
+    const btnMore = document.querySelector('.btn--more');
+    const text = document.querySelector('.build .build-text');
+
+    btnMore.addEventListener('click', function (e) {
+        text.classList.toggle('excerpt');
+    })
+});
