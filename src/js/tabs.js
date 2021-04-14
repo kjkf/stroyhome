f577271a032244bd6c6a991aa1cb851079f6140a
Lola <yakjkf@gmail.com>
2021-04-14 14:56:30 +0600

    block08 - tabs

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,27 +1,29 @@
 function initTabs() {
-    const tabs = document.querySelector('.tabs');
-    const tabItem = tabs.querySelectorAll('.tabs__item');
-    let activeTab = tabs.querySelector(".tabs__block.active");
-    let activeTabItem = tabs.querySelector(".tabs__item.active");
+    const tabsArray = document.querySelectorAll('.tabs');
+    tabsArray.forEach(function (tabs) {
+        const tabItem = tabs.querySelectorAll('.tabs__item');
+        let activeTab = tabs.querySelector(".tabs__block.active");
+        let activeTabItem = tabs.querySelector(".tabs__item.active");
 
-    tabItem.forEach(function(tab){
-        tab.addEventListener('click', function (e) {
-            e.preventDefault();
-            if (tab === activeTabItem) return;
-            activeTabItem.classList.remove('active');
-            tab.classList.add('active');
-            activeTabItem = tab;
+        tabItem.forEach(function(tab){
+            tab.addEventListener('click', function (e) {
+                e.preventDefault();
+                if (tab === activeTabItem) return;
+                activeTabItem.classList.remove('active');
+                tab.classList.add('active');
+                activeTabItem = tab;
 
-            const newActiveSelector = `#${tab.dataset.tab}`;
+                const newActiveSelector = `#${tab.dataset.tab}`;
 
-            const newActive = tabs.querySelector(newActiveSelector);
+                const newActive = tabs.querySelector(newActiveSelector);
 
-            activeTab.classList.remove('active');
+                activeTab.classList.remove('active');
 
-            newActive.classList.add('active');
-            activeTab = newActive;
+                newActive.classList.add('active');
+                activeTab = newActive;
 
 
-        } )
+            } )
+        })
     })
 }
