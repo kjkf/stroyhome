91a5e644d0865b02f26c11a3f3df027d88f6ec3c
Lola <yakjkf@gmail.com>
2021-04-15 20:30:14 +0600

    schema block bug fixed

--- src/js/shemaDescr.js
+++ src/js/shemaDescr.js
@@ -1,5 +1,6 @@
 function initSchemasDescr() {
     const schemasDescrElems = document.querySelectorAll('.schema-collapse-wrapper');
+    const schema = document.querySelector('.schema');
    /* const innerWidth = width.innerWidth;
     const innerHeight = width.innerHeight;
     const vw = innerWidth / 100;
@@ -13,7 +14,22 @@ function initSchemasDescr() {
         const plus = wrapper.querySelector('.schema-collapse__btn');
         const descr = wrapper.querySelector('.schema-collapse__descr');
 
-        plus.addEventListener('click', e => descr.classList.toggle('hide'));
-});
+        const wrapperHeight = schema.getBoundingClientRect().height;
+
+        const topInpx = wrapperHeight * wrapper.dataset.top / 100;
+
+
+        plus.addEventListener('click', e => {
+                descr.classList.toggle('hide');
+                const descrHeight = descr.getBoundingClientRect().height;
+                const bottomInpx = topInpx + descrHeight;
+
+    //console.log(wrapperHeight, descrHeight, topInpx, bottomInpx);
+                if (bottomInpx > wrapperHeight) {
+                    descr.classList.add('up');
+                    descr.style.top = `${-descrHeight}px`;
+                }
+        });
+    });
 
 };
