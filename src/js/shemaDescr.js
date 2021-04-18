f5447473f566d8f31e95517a67f79e9cd030edc7
Lola <yakjkf@gmail.com>
2021-04-18 18:36:29 +0600

    some bugs fix

--- src/js/shemaDescr.js
+++ src/js/shemaDescr.js
@@ -1,10 +1,6 @@
 function initSchemasDescr() {
     const schemasDescrElems = document.querySelectorAll('.schema-collapse-wrapper');
     const schema = document.querySelector('.schema');
-   /* const innerWidth = width.innerWidth;
-    const innerHeight = width.innerHeight;
-    const vw = innerWidth / 100;
-    const vh = innerHeight / 100;*/
 
     schemasDescrElems.forEach(wrapper => {
         //let left = +wrapper.dataset.left / vw;
@@ -14,7 +10,7 @@ function initSchemasDescr() {
         const plus = wrapper.querySelector('.schema-collapse__btn');
         const descr = wrapper.querySelector('.schema-collapse__descr');
 
-        const wrapperHeight = schema.getBoundingClientRect().height;
+        const wrapperHeight = Math.max(schema.getBoundingClientRect().height, 500);
 
         const topInpx = wrapperHeight * wrapper.dataset.top / 100;
 
@@ -23,6 +19,7 @@ function initSchemasDescr() {
                 descr.classList.toggle('hide');
                 const descrHeight = descr.getBoundingClientRect().height;
                 const bottomInpx = topInpx + descrHeight;
+    console.log(descrHeight, bottomInpx, wrapperHeight);
 
     //console.log(wrapperHeight, descrHeight, topInpx, bottomInpx);
                 if (bottomInpx > wrapperHeight) {
