287284e1ab6c5d03661eff2d3fd3ce6c956e2cda
Lola <yakjkf@gmail.com>
2021-04-26 15:42:00 +0600

    ipad bugs fixup

--- src/js/anim.js
+++ src/js/anim.js
@@ -1,12 +1,14 @@
 
 function init_titles_anim() {
    const titles = document.querySelectorAll('.title-anim');
-    if (!titles) return;
+    if (titles.length == 0) return;
     let bannerTitle = document.querySelector('.banner .title-anim');
 
-    titles.forEach(function (title) {
+    /*titles.forEach(function (title) {
         anime(title);
-    })
+    })*/
+    window.addEventListener('scroll', animOnScroll);
+    animOnScroll();
 };
 
 function anime(elem){
@@ -18,4 +20,34 @@ function anime(elem){
             elem.classList.add('fade_in')
         }
     });
+}
+
+function animOnScroll() {
+    const animItems = document.querySelectorAll('.title-anim');
+    for (let index = 0; index < animItems.length; index++) {
+        const animItem = animItems[index];
+        const animItemHeight = animItem.offsetHeight;
+        const animItemOffset = offset(animItem).top;
+        const animStart = 4;
+
+        let animItemPoint = window.innerHeight - animItemHeight / animStart;
+        if (animItemHeight > window.innerHeight) {
+            animItemPoint = window.innerHeight - window.innerHeight / animStart;
+        }
+
+        if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+            animItem.classList.add('fade_in')
+        }
+    };
+}
+
+function offset(el) {
+    const  rect = el.getBoundingClientRect(),
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    return {
+        top: rect.top + scrollTop,
+        left: rect.left + scrollLeft
+    }
 }
